62a69478f1362feb78eba7ff01e7b16d9786abdc
Frank Duckart <duckart@computer.org>
2016-11-17 14:06:13 -1000

    Remove an unneeded comment.

--- src/main/java/edu/hawaii/its/holiday/service/EmployeeServiceImpl.java
+++ src/main/java/edu/hawaii/its/holiday/service/EmployeeServiceImpl.java
@@ -8,12 +8,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * A JPA-based implementation of the Employee Service. Delegates to a JPA entity
- * manager to issue data access calls against the backing repository. The
- * EntityManager reference is provided by the managing container (Spring)
- * automatically.
- */
 @Service("employeeService")
 @Repository
 public class EmployeeServiceImpl implements EmployeeService {
