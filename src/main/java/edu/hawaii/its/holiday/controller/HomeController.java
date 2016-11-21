3424783991455b18f69c5e24a234b4ad47af258d
Frank Duckart <duckart@computer.org>
2016-11-21 10:59:08 -1000

    Remove unused methods.

--- src/main/java/edu/hawaii/its/holiday/controller/HomeController.java
+++ src/main/java/edu/hawaii/its/holiday/controller/HomeController.java
@@ -1,6 +1,5 @@
 package edu.hawaii.its.holiday.controller;
 
-import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -9,25 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.hawaii.its.holiday.access.User;
 import edu.hawaii.its.holiday.security.UserContextService;
-import edu.hawaii.its.holiday.service.HolidayService;
-import edu.hawaii.its.holiday.type.Holiday;
-import edu.hawaii.its.holiday.type.YearHolidayHolder;
-import edu.hawaii.its.holiday.util.Dates;
 
 @Controller
 public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-    @Autowired
-    private HolidayService holidayService;
 
     @Autowired
     private UserContextService userContextService;
@@ -52,29 +42,6 @@ public class HomeController {
         return "attributes";
     }
 
-    @ModelAttribute("holidayHolder")
-    public YearHolidayHolder createYearHolidayHolder(Integer year) {
-        List<Holiday> list = holidayService.findHolidays();
-
-        YearHolidayHolder holder = new YearHolidayHolder(list);
-        if (year != null) {
-            holder.setYear(year);
-        }
-
-        return holder;
-    }
-
-    protected Integer yearValue(BindingResult result) {
-        Integer year;
-        try {
-            String yearStr = (String) result.getFieldValue("year");
-            year = Integer.valueOf(yearStr);
-        } catch (Exception ex) {
-            year = Integer.valueOf(Dates.currentYear());
-        }
-        return year;
-    }
-
     @RequestMapping(value = "/contact", method = RequestMethod.GET)
     public String contact(Locale locale, Model model) {
         return "contact";
@@ -95,7 +62,4 @@ public class HomeController {
         return "redirect:/";
     }
 
-    public void setUserContextService(UserContextService userContextService) {
-        this.userContextService = userContextService;
-    }
 }
